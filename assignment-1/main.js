d07c3a66716b7ddf345427caf9b87027eed2f06c
jaywnz <64354555+jaywnz@users.noreply.github.com>
2021-03-28 22:32:17 +1300

    add server-side validation and form processing to JSON

--- assignment-1/main.js
+++ assignment-1/main.js
@@ -3,20 +3,46 @@
 
 "use strict";
 
-var select = document.getElementById("symbol");
-var placeholder = document.createElement("option");
+let select = document.getElementById("symbol");
+let placeholder = document.createElement("option");
+let table = document.getElementById("portfolio");
+
+// Construct placeholder option for select dropdown
 placeholder.text = "Select stock";
 placeholder.disabled = true;
 placeholder.selected = true;
-
 select.appendChild(placeholder);
+
+// // Construct first row of table with headers
+// row = document.createElement("tr");
+// symbolHead = document.createElement("th");
+// quantityHead = document.createElement("th");
+// priceHead = document.createElement("th");
+// table.appendChild(row);
+// row.appendChild(symbolHead);
+// row.appendChild(quantityHead);
+// row.appendChild(priceHead);
+
+// // Create table from portfolio.json
+// fetch("http://localhost:8080/portfolio.json")
+//     .then(response => response.json())
+//     .then(data => {
+//         let cell;
+//         for (let j = 0; j < data.length; j++){
+//             cell = document.createElement("td")
+
+
+
+//         }
+        
+//     });
 
 // Populate select with stock symbols from cs.json
 fetch("http://localhost:8080/cs.json")
     .then(response => response.json())
     .then(data => {
-        var opt;
-        for (var i = 0; i < data.length; i++){
+        let opt;
+        for (let i = 0; i < data.length; i++){
             opt = document.createElement("option");
             opt.text = data[i].symbol;
             opt.value = data[i].symbol;
