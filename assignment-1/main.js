d7e0e8ce6927b70a49f4e942e9eddade165e9b26
jaywnz <64354555+jaywnz@users.noreply.github.com>
2021-03-29 21:10:22 +1300

    create stock page

--- assignment-1/main.js
+++ assignment-1/main.js
@@ -3,66 +3,69 @@
 
 "use strict";
 
-let select = document.getElementById("symbol");
-let placeholder = document.createElement("option");
-let table = document.getElementById("portfolio");
-let currency = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 2
-})
+function makeTable() {
+    let table = document.getElementById("portfolio");
+    let currency = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2
+    })
+    // Construct first row of table with headers
+    let row = document.createElement("tr");
+    let stockHead = document.createElement("th");
+    let quantityHead = document.createElement("th");
+    let priceHead = document.createElement("th");
+    let gainHead = document.createElement("th");
+    stockHead.innerHTML = "Stock";
+    quantityHead.innerHTML = "Quantity";
+    priceHead.innerHTML = "Price";
+    gainHead.innerHTML = "Gain/Loss";
+    table.appendChild(row);
+    row.appendChild(stockHead);
+    row.appendChild(quantityHead);
+    row.appendChild(priceHead);
+    row.appendChild(gainHead);
 
-// Construct placeholder option for select dropdown
-placeholder.text = "Select stock";
-placeholder.disabled = true;
-placeholder.selected = true;
-select.appendChild(placeholder);
+    // Create table from portfolio.json
+    fetch("http://localhost:8080/portfolio.json")
+        .then(response => response.json())
+        .then(data => {
+            for (let j = 0; j < data.length; j++){
+                let row2 = document.createElement("tr");
+                let cell1 = document.createElement("td");
+                let cell2 = document.createElement("td");
+                let cell3 = document.createElement("td");
+                let cell4 = document.createElement("td");
+                
+                cell1.innerHTML = data[j].symbol;
+                cell2.innerHTML = data[j].quantity;
+                // Style price to USD for US stocks
+                cell3.innerHTML = currency.format(data[j].price);
+                cell4.innerHTML = data[j].gain;
 
-// Construct first row of table with headers
-let row = document.createElement("tr");
-let stockHead = document.createElement("th");
-let quantityHead = document.createElement("th");
-let priceHead = document.createElement("th");
-let gainHead = document.createElement("th");
-stockHead.innerHTML = "Stock";
-quantityHead.innerHTML = "Quantity";
-priceHead.innerHTML = "Price";
-gainHead.innerHTML = "Gain/Loss";
-table.appendChild(row);
-row.appendChild(stockHead);
-row.appendChild(quantityHead);
-row.appendChild(priceHead);
-row.appendChild(gainHead);
+                table.appendChild(row2);
+                row2.appendChild(cell1);
+                row2.appendChild(cell2);
+                row2.appendChild(cell3);
+                row2.appendChild(cell4);
+            }
 
-// Create table from portfolio.json
-fetch("http://localhost:8080/portfolio.json")
-    .then(response => response.json())
-    .then(data => {
-        for (let j = 0; j < data.length; j++){
-            let row2 = document.createElement("tr");
-            let cell1 = document.createElement("td");
-            let cell2 = document.createElement("td");
-            let cell3 = document.createElement("td");
-            let cell4 = document.createElement("td");
-            
-            cell1.innerHTML = data[j].symbol;
-            cell2.innerHTML = data[j].quantity;
-            // Style price to USD for US stocks
-            cell3.innerHTML = currency.format(data[j].price);
-            cell4.innerHTML = data[j].gain;
+        });
+}
 
-            table.appendChild(row2);
-            row2.appendChild(cell1);
-            row2.appendChild(cell2);
-            row2.appendChild(cell3);
-            row2.appendChild(cell4);
-        }
+function listSymbols() {
+    let select = document.getElementById("symbol");
+    let placeholder = document.createElement("option");
 
-    });
+    // Construct placeholder option for select dropdown
+    placeholder.text = "Select stock";
+    placeholder.disabled = true;
+    placeholder.selected = true;
+    select.appendChild(placeholder);
 
-// Populate select with stock symbols from cs.json
-fetch("http://localhost:8080/cs.json")
+    // Populate select with stock symbols from cs.json
+    fetch("http://localhost:8080/cs.json")
     .then(response => response.json())
     .then(data => {
         let opt;
@@ -73,3 +76,12 @@ fetch("http://localhost:8080/cs.json")
             select.appendChild(opt)
         }
     });
+}
+
+if (document.getElementById("portfolio")){
+    window.onload = makeTable();
+    window.onload = listSymbols();
+}
+else {
+    window.onload = listSymbols();
+}
