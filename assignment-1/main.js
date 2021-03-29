fbf5156fbcc57be26450be1ff54fd83230b1b355
jaywnz <64354555+jaywnz@users.noreply.github.com>
2021-03-29 14:44:23 +1300

    add calculated gain/loss to portfolio

--- assignment-1/main.js
+++ assignment-1/main.js
@@ -6,6 +6,12 @@
 let select = document.getElementById("symbol");
 let placeholder = document.createElement("option");
 let table = document.getElementById("portfolio");
+let currency = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2
+})
 
 // Construct placeholder option for select dropdown
 placeholder.text = "Select stock";
@@ -13,29 +19,47 @@ placeholder.disabled = true;
 placeholder.selected = true;
 select.appendChild(placeholder);
 
-// // Construct first row of table with headers
-// row = document.createElement("tr");
-// symbolHead = document.createElement("th");
-// quantityHead = document.createElement("th");
-// priceHead = document.createElement("th");
-// table.appendChild(row);
-// row.appendChild(symbolHead);
-// row.appendChild(quantityHead);
-// row.appendChild(priceHead);
+// Construct first row of table with headers
+let row = document.createElement("tr");
+let stockHead = document.createElement("th");
+let quantityHead = document.createElement("th");
+let priceHead = document.createElement("th");
+let gainHead = document.createElement("th");
+stockHead.innerHTML = "Stock";
+quantityHead.innerHTML = "Quantity";
+priceHead.innerHTML = "Price";
+gainHead.innerHTML = "Gain/Loss";
+table.appendChild(row);
+row.appendChild(stockHead);
+row.appendChild(quantityHead);
+row.appendChild(priceHead);
+row.appendChild(gainHead);
 
-// // Create table from portfolio.json
-// fetch("http://localhost:8080/portfolio.json")
-//     .then(response => response.json())
-//     .then(data => {
-//         let cell;
-//         for (let j = 0; j < data.length; j++){
-//             cell = document.createElement("td")
+// Create table from portfolio.json
+fetch("http://localhost:8080/portfolio.json")
+    .then(response => response.json())
+    .then(data => {
+        for (let j = 0; j < data.length; j++){
+            let row2 = document.createElement("tr");
+            let cell1 = document.createElement("td");
+            let cell2 = document.createElement("td");
+            let cell3 = document.createElement("td");
+            let cell4 = document.createElement("td");
+            
+            cell1.innerHTML = data[j].symbol;
+            cell2.innerHTML = data[j].quantity;
+            // Style price to USD for US stocks
+            cell3.innerHTML = currency.format(data[j].price);
+            cell4.innerHTML = data[j].gain;
 
+            table.appendChild(row2);
+            row2.appendChild(cell1);
+            row2.appendChild(cell2);
+            row2.appendChild(cell3);
+            row2.appendChild(cell4);
+        }
 
-
-//         }
-        
-//     });
+    });
 
 // Populate select with stock symbols from cs.json
 fetch("http://localhost:8080/cs.json")
